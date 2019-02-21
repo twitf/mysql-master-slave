35ebed110bf1a1fffd0db38e049768486f556b90
shiqiang <qshi@suntekcorps.com>
2019-02-21 13:33:18 +0800

    修改初始化sql

--- init.sql
+++ init.sql
@@ -1,12 +1,13 @@
 -- 主
-GRANT ALL PRIVILEGES ON *.* TO 'slave' @'172.18.0.2' IDENTIFIED BY 'slave' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'slave' @'%' IDENTIFIED BY 'slave' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 SHOW MASTER STATUS;
 
 -- 从
 STOP SLAVE;
-CHANGE MASTER TO master_host = '172.18.0.3',
+CHANGE MASTER TO master_host = '172.19.0.3',
 master_user = 'slave',
 master_password = 'slave',
-master_log_file = '';
-STOP START;
+master_log_file = 'xxxx';
+START SLAVE;
+SHOW SLAVE STATUS;
